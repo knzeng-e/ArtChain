ef27f5e9df347783ce901097d1b77828be656cdc
Th0t-Nz1g <kevin.nzeng@gmail.com>
2020-08-05 14:11:52 +0200

    cleanUp: removing unused files

--- contracts/DerWanderer.sol
+++
@@ -1,8 +0,0 @@
-// contracts/DerWanderer.sol
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.6.1;
-
-import "../node_modules/@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "../node_modules/@openzeppelin/contracts/utils/Counters.sol";
-
-
